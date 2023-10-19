fffc91c7bb16801b77e7e10c9da834320653d95a
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-10-19 12:31:22 +0200

    Update FileCopyExample.java

--- javatest/FileCopyExample.java
+++ javatest/FileCopyExample.java
@@ -2,9 +2,22 @@ import java.io.*;
 
 public class FileCopyExample {
     public static void main(String[] args) {
+        // Print the current directory
+        System.out.println("Current Directory: " + System.getProperty("user.dir"));
+
+        // List the contents of the javatest directory
+        File javatestDirectory = new File("/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest");
+        String[] contents = javatestDirectory.list();
+        System.out.println("Contents of javatest directory:");
+        if (contents != null) {
+            for (String file : contents) {
+                System.out.println(file);
+            }
+        }
+
         // Use absolute paths
-        String sourcePath = "/home/runner/work/JavaSE-49.java.io/javatest/input.txt";
-        String destinationPath = "/home/runner/work/JavaSE-49.java.io/javatest/output.txt";
+        String sourcePath = "/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest/input.txt";
+        String destinationPath = "/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest/output.txt";
 
         try (InputStream inputStream = new FileInputStream(sourcePath);
              OutputStream outputStream = new FileOutputStream(destinationPath)) {
