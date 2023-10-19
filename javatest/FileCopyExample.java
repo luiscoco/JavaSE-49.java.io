503665a111fa9903730ca3e2367f17892f655f48
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-10-19 12:21:03 +0200

    Update FileCopyExample.java

--- javatest/FileCopyExample.java
+++ javatest/FileCopyExample.java
@@ -1,20 +1,19 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class FileCopyExample {
     public static void main(String[] args) {
         // Print the current directory
         System.out.println("Current Directory: " + System.getProperty("user.dir"));
 
-        Path sourcePath = Paths.get("javatest/input.txt");
-        Path destinationPath = Paths.get("javatest/output.txt");
+        // Use absolute paths
+        Path sourcePath = Paths.get(System.getProperty("user.dir"), "javatest", "input.txt");
+        Path destinationPath = Paths.get(System.getProperty("user.dir"), "javatest", "output.txt");
 
         try {
-            Files.copy(sourcePath, destinationPath);
+            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
             System.out.println("File copied successfully.");
         } catch (IOException e) {
             e.printStackTrace();
