064063cbd576744b93d6e9e13ff7e553bb2ded51
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-10-19 12:10:46 +0200

    Update FileCopyExample.java

--- javatest/FileCopyExample.java
+++ javatest/FileCopyExample.java
@@ -1,32 +1,22 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileCopyExample {
     public static void main(String[] args) {
-
         // Print the current directory
         System.out.println("Current Directory: " + System.getProperty("user.dir"));
 
-        try (             
-             //These are the paths for Linux operating system
-             FileInputStream fis = new FileInputStream("javatest/input.txt");
-             FileOutputStream fos = new FileOutputStream("javatest/output.txt")) 
+        Path sourcePath = Paths.get("javatest/input.txt");
+        Path destinationPath = Paths.get("javatest/output.txt");
 
-             //These are the paths for Windows operating system
-             //FileInputStream fis = new FileInputStream("C:\\javatest\\input.txt");
-             //FileOutputStream fos = new FileOutputStream("C:\\javatest\\output.txt")) 
-        {
-
-            int byteData;
-            while ((byteData = fis.read()) != -1) {
-                fos.write(byteData);
-            }
-
+        try {
+            Files.copy(sourcePath, destinationPath);
             System.out.println("File copied successfully.");
-
-        } 
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
