0a32a18b9008ba9d7ad2fc7d1298e6a8116cbf4a
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-10-19 12:02:34 +0200

    Create FileCopyExample.java

---
+++ javatest/FileCopyExample.java
@@ -0,0 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyExample {
+    public static void main(String[] args) {
+        try (             
+             //These are the paths for Linux operating system
+             FileInputStream fis = new FileInputStream("javatest/input.txt");
+             FileOutputStream fos = new FileOutputStream("javatest/output.txt")) 
+
+             //These are the paths for Windows operating system
+             //FileInputStream fis = new FileInputStream("C:\\javatest\\input.txt");
+             //FileOutputStream fos = new FileOutputStream("C:\\javatest\\output.txt")) 
+        {
+
+            int byteData;
+            while ((byteData = fis.read()) != -1) {
+                fos.write(byteData);
+            }
+
+            System.out.println("File copied successfully.");
+
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
