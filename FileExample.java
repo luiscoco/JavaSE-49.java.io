d213f20ab8232ecb7a5c41f09f21c68055262f6e
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-10-19 11:08:09 +0200

    Update FileExample.java

--- FileExample.java
+++ FileExample.java
@@ -2,7 +2,12 @@ import java.io.File;
 
 public class FileExample {
     public static void main(String[] args) {
-        File file = new File("C:\\javatest\\example.txt");
+        
+        //This path is used because the github action agent is running in Linux
+        File file = new File("C:/javatest/example.txt");
+        
+        //This is the path for running in window operating system
+        // File file = new File("C:\\javatest\\example.txt");
 
         // Check if the file exists
         if (file.exists()) {
