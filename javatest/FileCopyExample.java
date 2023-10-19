8e977be91e0c02100e7195a61679fe6413105920
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-10-19 12:37:51 +0200

    Update FileCopyExample.java

--- javatest/FileCopyExample.java
+++ javatest/FileCopyExample.java
@@ -1,36 +1,18 @@
-import java.io.*;
+import java.io.IOException;
+import java.nio.file.*;
 
 public class FileCopyExample {
     public static void main(String[] args) {
         // Print the current directory
         System.out.println("Current Directory: " + System.getProperty("user.dir"));
 
-        // List the contents of the javatest directory
-        File javatestDirectory = new File("/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest");
-        String[] contents = javatestDirectory.list();
-        System.out.println("Contents of javatest directory:");
-        if (contents != null) {
-            for (String file : contents) {
-                System.out.println(file);
-            }
-        }
-
         // Use absolute paths
-        String sourcePath = "/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest/input.txt";
-        String destinationPath = "/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest/output.txt";
+        Path sourcePath = Paths.get("/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest/input.txt");
+        Path destinationPath = Paths.get("/home/runner/work/JavaSE-49.java.io/JavaSE-49.java.io/javatest/output.txt");
 
-        try (InputStream inputStream = new FileInputStream(sourcePath);
-             OutputStream outputStream = new FileOutputStream(destinationPath)) {
-
-            byte[] buffer = new byte[1024];
-            int length;
-
-            while ((length = inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, length);
-            }
-
+        try {
+            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
             System.out.println("File copied successfully.");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
